36da9fd04c7eb8082317f9cde13146fa393c3ba3
Kolawole Muh'Fawaz <ayodejikolawole99@gmail.com>
2025-10-15 14:11:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9632075722-5c0e1",
+  "appId": "1:555584693227:web:a9c0dc2de7c7401895efae",
+  "apiKey": "AIzaSyBNhCx5dHbjWyDSWkIhGeyrNcR5OL0pfk4",
+  "authDomain": "studio-9632075722-5c0e1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "555584693227"
+};
